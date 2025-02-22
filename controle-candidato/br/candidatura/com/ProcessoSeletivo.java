e33125e9812ab812e6ed090652109ce8f4a5ccac
vgonsalez <gonsalez@yahoo.com>
2025-02-22 09:57:44 -0300

    case 3 - imprimindo lista de candidatos selecionados: concluido

--- controle-candidato/br/candidatura/com/ProcessoSeletivo.java
+++ controle-candidato/br/candidatura/com/ProcessoSeletivo.java
@@ -6,8 +6,21 @@ public class ProcessoSeletivo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		selecaoCandidatos();
+		imprimirSelecionados();
 	}
+	
+	static void imprimirSelecionados() {
+		String [] candidatos = { "Felipe", "Marcia", "Julia", "Paulo", "Augusto" };
+		System.out.println("Imprimindo a lista de candidatos informando o indice do elemento:");
+		for (int indice=0; indice < candidatos.length; indice++) {
+			System.out.println("O candidato de nº "+ (indice+1) +" é "+ candidatos[indice]);
+		}
+		System.out.println("\nForma abreviada de interação for each");
+		for(String candidato: candidatos) {
+			System.out.println("O candidato selecionado foi "+ candidato);
+		}
+	}
+	
 	static void selecaoCandidatos() {
 		String [] candidatos = { "Felipe", "Marcia", "Julia", "Paulo", "Augusto", "Monica", 
 				"Fabricio", "Mirela", "Daniela", "Jorge" };
