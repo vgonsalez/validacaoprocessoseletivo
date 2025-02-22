63940f68442653237ef27d1721c1a2e6778511f9
vgonsalez <gonsalez@yahoo.com>
2025-02-22 10:23:20 -0300

    desafio controle de fluxo

---
+++ DesafioControleFluxo/src/br/desafioControleFluxo/com/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package br.desafioControleFluxo.com;
+
+public class ParametrosInvalidosException extends Exception {
+	public ParametrosInvalidosException(String message) {
+		super(message);
+	}
+}
