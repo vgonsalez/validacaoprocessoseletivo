66e81d12c55979669fd4f0eac602228f075216ba
vgonsalez <gonsalez@yahoo.com>
2025-02-22 09:26:53 -0300

    criando um pequeno sistema para validacao de processo seletivo: case 1 finalizado

---
+++ controle-candidato/br/candidatura/com/ProcessoSeletivo.java
@@ -0,0 +1,22 @@
+package br.candidatura.com;
+
+public class ProcessoSeletivo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		analisarCandidato(1900.0);
+		analisarCandidato(2200.0);
+		analisarCandidato(2000.0);
+	}
+	static void analisarCandidato(double salarioPretendido) {
+		double salarioBase = 2000.0;
+		if(salarioBase > salarioPretendido) {
+			System.out.println("LIGAR PARA O CANDIDATO");
+		} else if(salarioBase == salarioPretendido){
+			System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+		} else {
+			System.out.println("AGUARDANDO RESULTADO DOS DEMAIS CANDIDATOS");
+		}
+	}
+
+}
